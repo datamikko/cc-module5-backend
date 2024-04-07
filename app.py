0761233b78a6d86224cc69071964b8ff8e394742
Mikko Pajula <mikko.pajula@lapinamk.fi>
2024-04-07 14:37:22 +0300

    changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     # CODE HERE
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World changes!</p>"
 
 
 if __name__ == "__main__":
